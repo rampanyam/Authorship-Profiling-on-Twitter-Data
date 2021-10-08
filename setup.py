2bead56b28222fbec6cc2b10f032451253824563
rampanyam <47374802+rampanyam@users.noreply.github.com>
2021-10-08 19:11:52 +1100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Authorship Analysis is one of the latest areas of investigation in Natural Language Processing. The objective of authorship analysis is to distinguish the texts or articles written by the authors into different classes or categories by analysing the writing styles of various authors. The three main components involved in Authorship analysis are Author Attribution, Author Verification and Author profiling.',
+    author='Aditya-Kunagu',
+    license='',
+)
